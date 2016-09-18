45235ade882988523c13a9416e2bae5a08a051ff
rajeevmenonk <k.rajivmenon1991@gmail.com>
2016-09-18 00:32:00 -0400

    updated heapsort

--- heapSort.cpp
+++ heapSort.cpp
@@ -1,25 +1,103 @@
 #include <iostream>
 
-int parent ( int child)
+void swap (int &a, int &b)
 {
-    int parent = child / 2;
-    if ( child  % 2 == 0)
-        parent--;
-    return parent;
+   int temp = a;
+   a = b;
+   b = temp;
 }
 
-int childLeft ( int parent)
+
+void printArray (int *arr, int length)
 {
-    return (parent * 2 + 1);
+    std::cout << "\n";
+    for (int i = 0; i < length; i++)
+    {
+        std::cout << arr[i] << " " ;
+    }
+    std::cout << "\n";
 }
 
-int childRight ( int parent)
+#define parent(child) ((child % 2 == 0 ? (child/2 -1) : (child/2)))
+#define childLeft(parentNode) ((parentNode * 2 + 1))
+#define childRight(parentNode) ((parentNode * 2 + 2))
+
+void maxHeapify (int *arr, int pos, int length)
 {
-    return (parent * 2 + 2);
+    int leftChild = childLeft(pos);
+    int rightChild = childRight(pos);
+    if (rightChild >= length || leftChild >= length)
+        return;
+
+    int biggerValue = leftChild;
+    if (arr[rightChild] > arr[leftChild])
+        biggerValue = rightChild;
+
+    if (arr[pos] < arr[biggerValue])
+    {
+         swap(arr[pos], arr[biggerValue]);
+         maxHeapify(arr, biggerValue, length);
+    }
+}
+
+void buildHeap ( int *arr, int length)
+{
+    int iter = length/2 - 1;
+    while (iter >= 0)
+    {
+        maxHeapify(arr, iter, length);
+        iter--;
+    }
+}
+
+void heapSort ( int *arr, int length)
+{
+    buildHeap(arr, length );
+    for (int i = length - 1; i > 0; i--)
+    {
+        swap( arr[0], arr[i] );
+        maxHeapify( arr, 0, i);
+    }
+}
+
+void percolateUp (int *arr, int pos)
+{
+    int parentIter = parent(pos);
+
+    if(parentIter == -1)
+        return;
+
+    if ( arr[pos] > arr[parentIter] )
+    {
+       swap( arr[pos], arr[parentIter] );
+       percolateUp( arr, parentIter);
+    }
+}
+
+void insert (int *arr, int pos)
+{
+    percolateUp(arr, pos);
+}
+
+void increaseKey (int *arr, int pos, int newValue)
+{
+    arr[pos] = newValue;
+    percolateUp(arr, pos);
 }
 
 int main ()
 {
-    int i = 0;
+    #define LENGTH 12
+    int arr[] = {2, 4, 3, 16, 25, 15, 9, 21, 5, 1, 8, 7};
+    heapSort (arr, LENGTH);
+    printArray (arr, LENGTH);
+    
+    int arr1[] = {25, 21, 15, 16, 30, 7, 9, 2, 5, 1, 4, 3};
+    percolateUp(arr1, 4);
+    printArray (arr1, LENGTH);
 
+    increaseKey(arr1, 11, 40);
+    printArray (arr1, LENGTH);
+
+    return 0;
 }
